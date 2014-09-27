4dcb5d5d55dd595121903a804dad4f09aadfe281
simonorlovsky <simon.orlovsky@yahoo.com>
2014-09-27 12:34:29 -0500

    Implemented autorefresh for newsfeedTableView. It is awesomegit add .

--- Karma/NewsfeedViewController.m
+++ Karma/NewsfeedViewController.m
@@ -12,6 +12,7 @@
 @interface NewsfeedViewController ()
 {
     NSMutableArray * _completed;
+    UIRefreshControl* _refreshControl;
 }
 @end
 
@@ -19,6 +20,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Initialize the refresh control.
+    _refreshControl = [[UIRefreshControl alloc] init];
+    [self.tableView addSubview:_refreshControl];
+    _refreshControl.backgroundColor = [UIColor purpleColor];
+    _refreshControl.tintColor = [UIColor whiteColor];
+    [_refreshControl addTarget:self
+                            action:@selector(reloadData)
+                  forControlEvents:UIControlEventValueChanged];
+    
     self.tableView.delegate= self;
     self.tableView.dataSource= self;
     
@@ -45,7 +56,35 @@
         [_tableView reloadData];
         
     }];
+    
 
+
+}
+
+-(void)reloadData{
+    _completed = [[NSMutableArray alloc] init];
+    PFQuery *query = [PFQuery queryWithClassName:@"completed"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        
+        if (!error) {
+            // The find succeeded.
+            NSLog(@"Successfully retrieved %ld requests.", objects.count);
+            // Do something with the found objects
+            for (PFObject *object in objects) {
+                NSLog(@"%@", object.objectId);
+                [_completed insertObject:object[@"type"] atIndex:0];
+                //NSLog(_completed[0]);
+                
+            }
+        } else {
+            // Log details of the failure
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+        
+        [_tableView reloadData];
+        
+    }];
+    [_refreshControl endRefreshing];
 }
 
 - (void)didReceiveMemoryWarning {

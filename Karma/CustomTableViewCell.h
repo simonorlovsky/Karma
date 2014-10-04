df3c1a0ad5a214ee1b62ebe51e0f0362296722cf
simonorlovsky <simon.orlovsky@yahoo.com>
2014-10-04 14:10:12 -0500

    added some nice UI elements

---
+++ Karma/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  Karma
+//
+//  Created by Simon Orlovsky on 10/4/14.
+//  Copyright (c) 2014 simonorlovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+@end
